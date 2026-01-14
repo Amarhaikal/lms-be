3b38c6a332a5a77d168a6346bfd22b997553665d
Muhammad Amar Haikal bin Ahmad <amarhaikal@Muhammads-MacBook-Air.local>
2026-01-14 22:13:56 +0800

    add col address_id, descrease max length id_no

---
+++ Migrations/20260114140708_ReorderAddressIdColumn.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LMS.Migrations
+{
+    /// <inheritdoc />
+    public partial class ReorderAddressIdColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "id_no",
+                table: "users",
+                type: "varchar(10)",
+                maxLength: 10,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(70)",
+                oldMaxLength: 70)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            // Reorder address_id column to appear after phone_no
+            migrationBuilder.Sql(@"
+                ALTER TABLE users 
+                MODIFY COLUMN address_id INT NULL 
+                AFTER phone_no;
+            ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            // Move address_id back to the end (after updated_at)
+            migrationBuilder.Sql(@"
+                ALTER TABLE users 
+                MODIFY COLUMN address_id INT NULL 
+                AFTER updated_at;
+            ");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "id_no",
+                table: "users",
+                type: "varchar(70)",
+                maxLength: 70,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(10)",
+                oldMaxLength: 10)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
---
+++ Migrations/20260114141255_UpdateIdNoMaxLength.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LMS.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateIdNoMaxLength : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "id_no",
+                table: "users",
+                type: "varchar(12)",
+                maxLength: 12,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(10)",
+                oldMaxLength: 10)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "id_no",
+                table: "users",
+                type: "varchar(10)",
+                maxLength: 10,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(12)",
+                oldMaxLength: 12)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
