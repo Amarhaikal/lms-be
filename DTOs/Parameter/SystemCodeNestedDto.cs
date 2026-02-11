55b435c00e54916ed5ff467bf7413fec50085be1
Amar Haikal <muhdamarhaikal@gmail.com>
2026-02-12 01:52:43 +0800

    remove id in SystemCodeNestedDto

--- DTOs/Parameter/SystemCodeNestedDto.cs
+++ DTOs/Parameter/SystemCodeNestedDto.cs
@@ -2,7 +2,6 @@ namespace QUANTM.DTOs.Parameter
 {
     public class SystemCodeNestedDto
     {
-        public int Id { get; set; }
         public string Code { get; set; } = null!;
         public string Description { get; set; } = null!;
     }
