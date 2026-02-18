858c4271a42d727e0d1f1c90349b501dff69a528
Amar Haikal <muhdamarhaikal@gmail.com>
2026-02-18 23:52:27 +0800

    adjust api get system code for filtering

---
+++ DTOs/Parameter/CodeTypeNestedDto.cs
@@ -0,0 +1,8 @@
+namespace QUANTM.DTOs.Parameter
+{
+    public class CodeTypeNestedDto
+    {
+        public string Code { get; set; } = null!;
+        public string Description { get; set; } = null!;
+    }
+}
